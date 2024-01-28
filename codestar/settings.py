f0e01227cc290c1f3e4d738259ec6634f3d370c7
vladar21 <vladar21@gmail.com>
2024-01-28 21:44:04 +0000

    further work with django_summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'django_summernote',
+    'blog',
 ]
 
 MIDDLEWARE = [
