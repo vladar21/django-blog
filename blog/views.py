17864bc8cd6f2b01f585f0644166d633e21bcc6c
vladar21 <vladar21@gmail.com>
2024-01-28 00:13:06 +0000

    ordering and filtering

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,7 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
+    # queryset = Post.objects.all().order_by("-created_on")
+    # queryset = Post.objects.filter(author=2)
+    # template_name = "post_list.html"
