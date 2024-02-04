5c238c7330cf0774b673dbdb83325f03b3bf7cd6
vladar21 <vladar21@gmail.com>
2024-02-04 17:15:29 +0000

    create the Collaboration Request form

--- blog/views.py
+++ blog/views.py
@@ -35,6 +35,7 @@ def post_detail(request, slug):
         comment_count = post.comments.filter(approved=True).count()
 
         if request.method == "POST":
+            print("Received a POST request")
             comment_form = CommentForm(data=request.POST)
             if comment_form.is_valid():
                 comment = comment_form.save(commit=False)
@@ -47,6 +48,8 @@ def post_detail(request, slug):
                 )
 
         comment_form = CommentForm()
+
+        print("About to render template")
 
         return render(
             request,
