6476374a4fa00b1d5585ae0d3e083c05adde3383
vladar21 <vladar21@gmail.com>
2024-02-05 22:03:50 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
